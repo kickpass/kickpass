e25ead6a1297c496ffdf73d3f753ba221691ed99
Paul Fariello <pfariello@arkoon.dev>
2015-05-07 12:09:01 +0200

    Use renamed editor scripts

--- test/create-subdir.sh
+++ test/create-subdir.sh
@@ -6,7 +6,7 @@ export EDITOR="$EDITOR_PATH/TestIntegrationEditorDate.sh"
 $KP init
 
 # When
-$EXPECT $SRC/create.expect create subdir/test
+$EXPECT $SRC/password-confirm.expect create subdir/test
 
 # Then
 [ -f $HOME/.kickpass/subdir/test ] || abort "missing kickpass safe"
--- test/create.sh
+++ test/create.sh
@@ -6,7 +6,7 @@ export EDITOR="$EDITOR_PATH/TestIntegrationEditorDate.sh"
 $KP init
 
 # When
-$EXPECT $SRC/create.expect create test
+$EXPECT $SRC/password-confirm.expect create test
 
 # Then
 [ -f $HOME/.kickpass/test ] || abort "missing kickpass safe"
